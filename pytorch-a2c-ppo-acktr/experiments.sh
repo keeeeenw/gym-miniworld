0021905effff0d1883032354a16af30f48970540
Zixiao Wang <wangxiaotian2007@gmail.com>
2020-10-05 18:58:02 -0700

    Updated one gru meta RL results

--- pytorch-a2c-ppo-acktr/experiments.sh
+++ pytorch-a2c-ppo-acktr/experiments.sh
@@ -2,20 +2,22 @@
 
 export DISPLAY=:0
 
-# Normal Learning rate meta
+# # Normal Learning rate meta
+# python3 main.py --algo ppometa --num-frames 10000000 --num-processes 48 --num-steps 80 --lr 0.00005 --env-name MiniWorld-FourRoomsMeta-v0 --recurrent-policy
+
+# # Normal Learning rate
+# python3 main.py --algo ppometa --num-frames 10000000 --num-processes 48 --num-steps 80 --lr 0.00005 --env-name MiniWorld-FourRooms-v0 --recurrent-policy
+
+# # Learning rate 0.00001
+# python3 main.py --algo ppometa --num-frames 10000000 --num-processes 48 --num-steps 80 --lr 0.00001 --env-name MiniWorld-FourRoomsMeta-v0 --recurrent-policy
+
+# # Learning rate 0.00003
+# python3 main.py --algo ppometa --num-frames 10000000 --num-processes 48 --num-steps 80 --lr 0.00003 --env-name MiniWorld-FourRoomsMeta-v0 --recurrent-policy
+
+# # Learning rate 0.0001
+# python3 main.py --algo ppometa --num-frames 10000000 --num-processes 48 --num-steps 80 --lr 0.0001 --env-name MiniWorld-FourRoomsMeta-v0 --recurrent-policy
+
+# # Learning rate 0.001
+# python3 main.py --algo ppometa --num-frames 10000000 --num-processes 48 --num-steps 80 --lr 0.001 --env-name MiniWorld-FourRoomsMeta-v0 --recurrent-policy
+
 python3 main.py --algo ppometa --num-frames 10000000 --num-processes 48 --num-steps 80 --lr 0.00005 --env-name MiniWorld-FourRoomsMeta-v0 --recurrent-policy
-
-# Normal Learning rate
-python3 main.py --algo ppometa --num-frames 10000000 --num-processes 48 --num-steps 80 --lr 0.00005 --env-name MiniWorld-FourRooms-v0 --recurrent-policy
-
-# Learning rate 0.00001
-python3 main.py --algo ppometa --num-frames 10000000 --num-processes 48 --num-steps 80 --lr 0.00001 --env-name MiniWorld-FourRoomsMeta-v0 --recurrent-policy
-
-# Learning rate 0.00003
-python3 main.py --algo ppometa --num-frames 10000000 --num-processes 48 --num-steps 80 --lr 0.00003 --env-name MiniWorld-FourRoomsMeta-v0 --recurrent-policy
-
-# Learning rate 0.0001
-python3 main.py --algo ppometa --num-frames 10000000 --num-processes 48 --num-steps 80 --lr 0.0001 --env-name MiniWorld-FourRoomsMeta-v0 --recurrent-policy
-
-# Learning rate 0.001
-python3 main.py --algo ppometa --num-frames 10000000 --num-processes 48 --num-steps 80 --lr 0.001 --env-name MiniWorld-FourRoomsMeta-v0 --recurrent-policy
